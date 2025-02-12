bb7dabaf7964f23afd8dcc5e23c3ef171b5d3184
tommyrharper <thomasrobertharper@gmail.com>
2025-02-12 11:41:43 +0000

    feat: start.py

---
+++ start.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# training_data = datasets.Fashion
+
+print('hello world')

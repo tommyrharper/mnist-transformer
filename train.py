9559ad98b0e11c4a51513ec3f4cdf6555d41293d
tommyrharper <thomasrobertharper@gmail.com>
2025-02-12 16:49:33 +0000

    feat: start training func with model.train

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn, optim
 from transformer import VisionTransformer
-from dataloader import four_digit_train
+from dataloader import train_dataloader, test_dataloader
 
 model = VisionTransformer()
 loss_fn = nn.CrossEntropyLoss()
@@ -9,10 +9,14 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.data)
-    print(size)
+def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, num_epochs=10):
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+    model.train()
+
+    for epoch in range(num_epochs):
+        pass
+    pass
 
 
 print('training...')
-train(four_digit_train, model, loss_fn, optimizer)
+train(train_dataloader, test_dataloader, model, loss_fn, optimizer)

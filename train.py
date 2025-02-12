22c86456be1b0ddce12fe6d91b838e5dc04bfb06
tommyrharper <thomasrobertharper@gmail.com>
2025-02-12 17:35:00 +0000

    refactor: extract calculate_loss

--- train.py
+++ train.py
@@ -10,6 +10,28 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
+
+def calculate_loss(images, labels, model, device, loss_fn):
+    images = images.to(device)
+    labels = labels.to(device)
+
+    digit_predictions = model(images)
+
+    loss = 0
+    for i in range(4):
+        loss += loss_fn(digit_predictions[i], labels[:, i])
+    loss = loss / 4
+
+    correct_digits = 0
+    digits_checked = 0
+    # Calculate accuracy
+    for i in range(4):
+        pred = digit_predictions[i].argmax(dim=1)
+        correct_digits += (pred == labels[:, i]).sum().item()
+        digits_checked += labels.size(0)
+
+    return loss, correct_digits, digits_checked
+
 def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, device, num_epochs=10):
     model = model.to(device)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
@@ -23,27 +45,15 @@ def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, device, 
         # for images, labels in train_dataloader:
         for images, labels in tqdm(train_dataloader, desc=f"Train Epoch {epoch + 1}"):
             # TODO: remove code duplication from here and in the validation loop
-            images = images.to(device)
-            labels = labels.to(device)
 
-            digit_predictions = model(images)
-
-            loss = 0
-            for i in range(4):
-                loss += loss_fn(digit_predictions[i], labels[:, i])
-            loss = loss / 4
+            loss, correct, total = calculate_loss(images, labels, model, device, loss_fn)
+            train_loss += loss.item()
+            correct_digits += correct
+            total_digits += total
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-            train_loss += loss.item()
-
-            # Calculate accuracy
-            for i in range(4):
-                pred = digit_predictions[i].argmax(dim=1)
-                correct_digits += (pred == labels[:, i]).sum().item()
-                total_digits += labels.size(0)
 
         avg_train_loss = train_loss / len(train_dataloader)
         train_accuracy = correct_digits / total_digits

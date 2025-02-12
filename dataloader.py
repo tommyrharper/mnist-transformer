691c1b3eea997e29e05cb6110772fe758a53411c
tommyrharper <thomasrobertharper@gmail.com>
2025-02-12 13:24:02 +0000

    refactor: inherit MNIST in FourDigitMNIST class

--- dataloader.py
+++ dataloader.py
@@ -1,35 +1,21 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import random
 
-mnist_training_data = datasets.MNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
-
-mnist_test_data = datasets.MNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor()
-)
-
-class FourDigitMNIST(Dataset):
-    def __init__(self, mnist_dataset):
-        self.mnist_dataset = mnist_dataset
+class FourDigitMNIST(datasets.MNIST):
+    def __init__(self, root, train, download, transform):
+        super().__init__(root=root, train=train, download=download, transform=transform)
 
     def __len__(self):
-        return len(self.mnist_dataset) // 4
+        return len(self.data) // 4
     
     def __getitem__(self, idx):
         # Get 4 random digits from the dataset
-        indices = random.sample(range(len(self.mnist_dataset)), 4)
-        digits = [self.mnist_dataset[i][0] for i in indices]  # Get images
-        labels = torch.tensor([self.mnist_dataset[i][1] for i in indices])  # Get labels as tensor [4,2,7,1]
+        indices = random.sample(range(len(self.data)), 4)
+        digits = [self.data[i] for i in indices]  # Get images
+        labels = torch.tensor([self.targets[i] for i in indices])  # Get labels as tensor [4,2,7,1]
         
         # Create composite image
         composite_image = torch.zeros(1, 56, 56)
@@ -39,11 +25,22 @@ class FourDigitMNIST(Dataset):
         composite_image[:, 28:, 28:] = digits[3]
         
         return composite_image, labels
+    
 
 batch_size = 32
 
-four_digit_train = FourDigitMNIST(mnist_training_data)
-four_digit_test = FourDigitMNIST(mnist_test_data)
+four_digit_train = FourDigitMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+four_digit_test = FourDigitMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
 
 train_dataloader = DataLoader(four_digit_train, batch_size=batch_size)
 test_dataloader = DataLoader(four_digit_test, batch_size=batch_size)

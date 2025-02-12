1250aee3e6e83d85d5aab141894f1460dffd8cf5
tommyrharper <thomasrobertharper@gmail.com>
2025-02-12 17:01:58 +0000

    feat: add epoch and dataloader loop in training

--- train.py
+++ train.py
@@ -9,14 +9,33 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, num_epochs=10):
+def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, device, num_epochs=10):
+    model = model.to(device)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
-    model.train()
 
     for epoch in range(num_epochs):
-        pass
+        model.train()
+        train_loss = 0
+
+        for images, labels in train_dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            digit_predictions = model(images)
+
+            # loss = 0
+            # for i in range(4):
+            #     loss += loss_fn(digit_predictions[i], labels[:, i])
+            # loss = loss / 4
+
+            # optimizer.zero_grad()
+            # loss.backward()
+            # optimizer.step()
+
+            # train_loss += loss.item()
+            pass
     pass
 
 
 print('training...')
-train(train_dataloader, test_dataloader, model, loss_fn, optimizer)
+train(train_dataloader, test_dataloader, model, loss_fn, optimizer, device)
